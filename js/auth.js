ed092e63890da0373957b559764b16e1639a2e32
SolarInco <Solarinco@outlook.com>
2026-08-10 22:45:19 -0400

    Redirect to home.html instead of chat.html after login

--- js/auth.js
+++ js/auth.js
@@ -74,7 +74,7 @@ authActionBtn.addEventListener('click', async () => {
                 status: "online"
             }, { merge: true });
 
-            window.location.href = "html/chat.html";
+            window.location.href = "html/home.html";
             
         } else {
             if (!username) {
@@ -91,7 +91,7 @@ authActionBtn.addEventListener('click', async () => {
                 status: "online"
             });
 
-            window.location.href = "html/chat.html";
+            window.location.href = "html/home.html";
         }
     } catch (error) {
         authError.textContent = error.message.replace("Firebase: ", "");
